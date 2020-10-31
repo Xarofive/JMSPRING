74f6a07f1604d03fa07dad8b1ec37d72fe1fc6d7
Ivan Pshychenko <68253439+Xarofive@users.noreply.github.com>
2020-10-31 18:48:14 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
